91827b2a6eac538b32fe51a96c5cca16150fe2c0
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-11-22 15:49:38 +0100

    cleaning branch adding cors

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import register_routes
 from .s3 import s3_client
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     s3_client_instance = s3_client()
     register_routes(app, s3_client_instance)
     
