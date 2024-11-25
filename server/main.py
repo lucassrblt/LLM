690883571aaf20fef467da27b09baf43d71bb477
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-11-25 14:49:02 +0100

    fix main, add dependencies

--- server/main.py
+++ server/main.py
@@ -1,6 +1,22 @@
-from __init__ import create_app
+from flask import Flask
+from flask_cors import CORS
+from routes import register_routes
+from s3 import s3_client
+from dotenv import load_dotenv
 
-app, _ = create_app()
+load_dotenv()
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://localhost:5174" ]}})
+s3_client_instance = s3_client()
+register_routes(app, s3_client_instance)
+
+@app.route('/')
+def index():
+    return "It works with Flaskkkkk!"
+
+application = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
